ce9c6f648bf0dfa274d5f5f218a33933d0eeb413
mike dupont <mike.dupont@introspector.local>
2024-07-29 08:37:40 -0400

    now training

--- autogluon_test.py
+++ autogluon_test.py
@@ -4,32 +4,28 @@ import glob
 import pandas as pd
 import re
 from autogluon.tabular import TabularDataset, TabularPredictor        
+MAXFILES=400000000
+MAXFIELDS=200000000
 
 df_fields = pd.read_csv("report_without_numbers_sorted.csv")
-#Unable to allocate 345. TiB for an array with shape (10138191, 4676243) and data type object
-#fields_ds = df_fields.head(10000)['index']
-#fields_ds = df_fields.head(100)['index']
-
-# 1000 = 600GB
-fields = { x:1 for x in df_fields.head(450)['index'].to_dict().values()} # 600
-
+fields = { x:1 for x in df_fields.head(MAXFIELDS)['index'].to_dict().values()} 
 truncated_data = []
-for name in glob.glob("pyprolog-asts/*")[0:100]:
+for name in glob.glob("pyprolog-asts/*")[0:MAXFILES]:
     try:
         ds = datasets.load_from_disk(name)
     except FileNotFoundError as e:
         continue # not a dataset
-    print(name)
-    
+    print(name)   
     for x in ds:
         print (x)
         ds2 =ds[x]
         iter=ds2.iter(batch_size=1)
         for i in iter:
+            c = {
+                "label" : name
+            } # the object to collect
+            
             for k in i:
-                c = {
-                    "label" : name
-                } # the object to collect
                 k2 = re.sub("\d+", "N", k)
                 dskip = 0
                 for skip in ("lineno","offset"):
@@ -40,19 +36,34 @@ for name in glob.glob("pyprolog-asts/*")[0:100]:
                         v = i[k][0] # get the value
                         if isinstance(v, (str,float,int)):
                             c[k2]=v # fold the fields together
-
-                truncated_data.append(c)
+            truncated_data.append(c)
 
 if True:                    
     df_pandas = pd.DataFrame(truncated_data)
     #df_pandas.to_parquet("first_dataset.parquet")
 
+    # try:
+    #     df_pandas.to_feather("first_dataset.fth")
+    # except Exception as e:
+    #     print(e)
+    # try:
+    #     df_pandas.to_parquet("first_dataset.parquet")
+    # except Exception as e:
+    #     print(e)
+    # try:
+    #     df_pandas.to_orc("first_dataset.orc")
+    # except Exception as e:
+    #     print(e)
     try:
-        df_pandas.to_feather("first_dataset.fth")
+        df_pandas.to_pickle("first_dataset3.pkl")
     except Exception as e:
         print(e)
     try:
-        df_pandas.to_csv("first_dataset.csv",escapechar="\\")
+        df_pandas.to_json("first_dataset3.json")
+    except Exception as e:
+        print(e)
+    try:
+        df_pandas.to_csv("first_dataset3.csv",escapechar="\\")
     except Exception as e:
         print(e)
         
